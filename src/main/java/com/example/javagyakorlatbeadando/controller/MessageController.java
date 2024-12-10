1457cd0765a7b5f2ed6068aa9bfd377c69fa8814
Jani1125 <janika1125@gmail.com>
2024-12-10 14:43:22 +0100

    admin üzenetkiírás működik

--- src/main/java/com/example/javagyakorlatbeadando/controller/MessageController.java
+++ src/main/java/com/example/javagyakorlatbeadando/controller/MessageController.java
@@ -1,11 +1,13 @@
 package com.example.javagyakorlatbeadando.controller;
 
+import com.example.javagyakorlatbeadando.repository.MessageRepository;
 import com.example.javagyakorlatbeadando.entity.Message;
 import com.example.javagyakorlatbeadando.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,6 +18,7 @@ public class MessageController {
 
     @Autowired
     private MessageService messageService;
+    private MessageRepository messageRepository;
 
     @GetMapping("/kapcsolat")
     public String showContactForm(Model model) {
@@ -35,5 +38,14 @@ public class MessageController {
         List<Message> messages = messageService.getAllMessages();
         model.addAttribute("messages", messages);
         return "admin";
+    }
+
+    @PostMapping("/admin/uzenetek/valasz/{id}")
+    public String saveReply(@RequestParam("valasz") String replyText,
+                            @PathVariable("id") Long messageId) {
+        Message message = messageRepository.findById(messageId).orElseThrow(() -> new IllegalArgumentException("Invalid message Id:" + messageId));
+        message.setValasz(replyText);
+        messageRepository.save(message);
+        return "redirect:/admin/uzenetek";
     }
 }
